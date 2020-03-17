e9d533b1854d2a3b081b677a6e4df28abe341349
Brendon Dulam <brendon.dulam@veoneer.com>
2020-03-18 04:18:07 +0900

    problem2 leap year solution

--- main.go
+++ main.go
@@ -2,25 +2,15 @@ package main
 
 import (
 	"fmt"
-	"veltra-challenge/problem1"
+	"veltra-challenge/problem2"
 )
 
 func main() {
-	words := []string{
-		"",
-		"?",
-		"0",
-		"A",
-		"AB",
-		"11",
-		"11a",
-		"113",
-		"Hello World",
-		"A Toyota’s a Toyota",
-		"Nurses run",
+	years := []int{
+		-1, 0, 4, 10, 20, 100, 1700, 1800, 1900, 1600, 2000, 2019, 2020,
 	}
 
-	for _, word := range words {
-		fmt.Println(problem1.CheckPalindrome(word), ":", word)
+	for _, year := range years {
+		fmt.Println(year, ":", problem2.IsLeapYear(year))
 	}
 }
