197b75ecdf3e7d1354238cb41519af6785146faf
Brendon Dulam <brendon.dulam@veoneer.com>
2020-03-18 10:39:16 +0900

    test drivers

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+	"veltra-challenge/problem1"
 	"veltra-challenge/problem2"
 )
 
-func main() {
+func p2TestDriver() {
 	years := []int{
 		-1, 0, 4, 10, 20, 100, 1700, 1800, 1900, 1600, 2000, 2019, 2020,
 	}
@@ -13,4 +14,34 @@ func main() {
 	for _, year := range years {
 		fmt.Println(year, ":", problem2.IsLeapYear(year))
 	}
+}
+
+func p1TestDriver() {
+	words := []string{
+		"",
+		"?",
+		"0",
+		"A",
+		"AB",
+		"11",
+		"11a",
+		"113",
+		"Hello World",
+		"A Toyota’s a Toyota",
+		"Nurses run",
+	}
+
+	for _, word := range words {
+		fmt.Println(problem1.CheckPalindrome(word), ":", word)
+	}
+}
+
+func main() {
+	fmt.Println("Problem 1")
+	p1TestDriver()
+
+	fmt.Println()
+
+	fmt.Println("Problem 2")
+	p2TestDriver()
 }
